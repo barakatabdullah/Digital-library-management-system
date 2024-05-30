cd6e46b3ca099bb8767fcfa9a93a90e28b046848
Abdullah Barakat <abdallah771363885@gmail.com>
2024-05-30 19:44:42 +0300

    feat( layout ): add layout

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,11 +1,24 @@
-export default function NavBar() {
+import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router-dom';
 
+export default function NavBar() {
+  const navigate = useNavigate();
 
     return (
       <>
-        <div className="nav-bar">
-            hello
+ <nav className="w-screen flex items-center justify-center  py-8 bg-[#111827]">
+        <div className="container mx-auto  flex items-center justify-between">
+
+            <div className="flex gap-4">
+                <Button outlined  text onClick={()=>navigate('/')}  label='Home' className="text-white"/>
+                <Button  text onClick={()=>navigate('loan')}  label='Loan' className="text-white"/>
+            </div>
+
+            <div>
+                <Button text icon="i-tabler-door-exit" className="text-white"/>
+            </div>
         </div>
+    </nav>
       </>
     )
   }
---
+++ front/src/pages/Layout.jsx
@@ -0,0 +1,15 @@
+
+import { Outlet } from 'react-router-dom'
+import NavBar from '../components/NavBar'
+
+export default function Layout() {
+
+
+  return (
+    <>
+      <NavBar/>
+      <Outlet/>
+    </>
+  )
+}
+
