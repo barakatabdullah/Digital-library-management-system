85875fe4799db0e67ccd379949a048281b5e3cea
Abdullah Barakat <abdallah771363885@gmail.com>
2024-05-30 21:52:51 +0300

    feat( config ): add alias resolver for cleaner code

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: { alias: { '@': '/src' } },
   server: {
-
-    proxy: {
-      
+    proxy: { 
       "/api": {
         target: "http://127.0.0.1:8000",
         changeOrigin: true,
